227b6b8f5042e47f5f1ad36da99c1ed81a98dd6a
polariseye <polariseye@126.com>
2019-07-04 23:26:33 +0800

    提交测试程序

---
+++ lib/containerTst.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: "container test",
+      color: Colors.amber ,
+      home: Center(
+        child: new Container(
+          constraints: new BoxConstraints.expand(
+            height:Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+          ),
+          decoration: new BoxDecoration(
+            border: new Border.all(width: 2.0, color: Colors.red),
+            color: Colors.grey,
+            borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+            image: new DecorationImage(
+              image: new NetworkImage('http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
+              centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+            ),
+          ),
+          padding: const EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          child: new Text('Hello World',
+              style: Theme.of(context).textTheme.display1.copyWith(color: Colors.black)),
+          transform: new Matrix4.rotationZ(0.3),
+        ),
+      ),
+    );
+  }
+
+}
